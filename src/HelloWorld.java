3828e59c1b71c23a7d8b32abb60020650ce862b8
Michael J Sigg <mjsigg@gmail.com>
2022-06-09 14:47:55 -0500

    first exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+//        System.out.print("Hello, World");
+//        System.out.print("Hello, World");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Okay";
+        System.out.println(myString);
+
+    }
+
+
+}
