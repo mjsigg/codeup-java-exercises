5e463a2a424eb8567d8fc3425264410f3faea9c9
Michael J Sigg <mjsigg@gmail.com>
2022-06-10 14:21:19 -0500

    EOD

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,13 +3,64 @@ public class HelloWorld {
 //        System.out.print("Hello, World");
 //        System.out.print("Hello, World");
 
-        int myFavoriteNumber = 7;
-        System.out.println(myFavoriteNumber);
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Okay";
+//        System.out.println(myString);
+//
+////        long myNumber = 123;
+////        System.out.println(myNumber);
+//
+//         float myNumber = (float) 3.14;
+//         double myNumber = 3.14;
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
 
-        String myString = "Okay";
-        System.out.println(myString);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        String name = "somekindofname";
+        System.out.println(name);
+
+//        int x = 4;
+//        x = x + 5;
+//
+//        x += 5;
+//
+//        ++x;
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//
+//        y = y * x;
+//
+//        y *= x;
+//        System.out.println(x);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        y /= x;
+
+        byte x = 127;
+
+//        double x = int 128;
+//        System.out.println(x);
 
     }
+
 
 
 }
